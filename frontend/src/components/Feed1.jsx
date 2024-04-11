906538cbde83d96b1750bf44bf8e64ceae2862f7
rupesh <rpiwal02@gmail.com>
2024-04-11 15:38:08 +0530

    tabs styling

---
+++ frontend/src/components/Feed1.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Feed1 = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, itaque vel ex eveniet et excepturi voluptatum tempore odio qui debitis, consectetur ab architecto repudiandae ullam quae nobis dolore quam animi?</p>
+
+    </div>
+  )
+}
+
+export default Feed1
--- frontend/src/components/FeedTabs.jsx
+++ frontend/src/components/FeedTabs.jsx
@@ -4,6 +4,7 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import { useState } from "react";
+import Feed1 from "./Feed1";
 
 const FeedTabs = () => {
   const [value, setValue] = useState("1");
@@ -15,16 +16,23 @@ const FeedTabs = () => {
   return (
     <Box sx={{ width: "100%", typography: "body1" }}>
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+          }}
+        >
           <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Item One" value="1" />
-            <Tab label="Item Two" value="2" />
-            <Tab label="Item Three" value="3" />
+            <Tab style={{ width: "50%" }} label="For You" value="1" />
+            <Tab style={{ width: "50%" }} label="Following" value="2" />
+            {/* <Tab label="Item Three" value="3" /> */}
           </TabList>
         </Box>
-        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="1">
+          <Feed1 />
+        </TabPanel>
         <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
+        {/* <TabPanel value="3">Item Three</TabPanel> */}
       </TabContext>
     </Box>
   );
