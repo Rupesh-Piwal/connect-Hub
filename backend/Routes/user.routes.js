4c9884484b88d49539d883c2c81c1a6ed3e57b08
rupesh <rpiwal02@gmail.com>
2024-04-28 23:03:01 +0530

    user routes

---
+++ backend/Routes/user.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute";
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.post("/update", protectRoute, updateUser);
+
+export default router;
