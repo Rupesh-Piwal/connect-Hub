8c8ff57d8105ac36b59f6197a696d39eff134bc1
rupesh <rpiwal02@gmail.com>
2024-05-03 11:55:50 +0530

    deleted post component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,44 @@
-import React from "react";
-import Navigations from "../components/Navigations.jsx";
-import Feed from "../components/Feed";
-import RightColumn from "../components/RightColumn";
+import { useState } from "react";
+import Posts from "../../components/common/Posts";
+import CreatePost from "./CreatePost";
 
 const Home = () => {
+  const [feedType, setFeedType] = useState("forYou");
+
   return (
-    <div className="grid lg:grid-cols-3  ">
-      <Navigations />
-      <Feed />
-      <RightColumn />
-    </div>
+    <>
+      <div className="flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen">
+        {/* Header */}
+        <div className="flex w-full border-b border-gray-700">
+          <div
+            className={
+              "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            }
+            onClick={() => setFeedType("forYou")}
+          >
+            For you
+            {feedType === "forYou" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+          <div
+            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            onClick={() => setFeedType("following")}
+          >
+            Following
+            {feedType === "following" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+        </div>
+
+        {/*  CREATE POST INPUT */}
+        <CreatePost />
+
+        {/* POSTS */}
+        <Posts />
+      </div>
+    </>
   );
 };
 
