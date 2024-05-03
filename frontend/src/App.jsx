ecf429bcc4b0c6b616e9830b443a5f4a4e51b35a
rupesh <rpiwal02@gmail.com>
2024-05-03 09:54:48 +0530

    page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,22 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Profile from "./pages/Profile.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
 
 const App = () => {
   return (
-    <>
+    <div className="flex max-w-6xl mx-auto">
       <Routes>
         <Route element={<Home />} path="/" />
+        <Route element={<Register />} path="/register" />
+        <Route element={<Login />} path="/login" />
         <Route element={<Profile />} path="/profile" />
       </Routes>
-    </>
+    </div>
   );
 };
 
 export default App;
+
+
