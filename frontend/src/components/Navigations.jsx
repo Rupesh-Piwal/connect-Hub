3f26b520d041769a204b85c6a2f06f032c90ead5
rupesh <rpiwal02@gmail.com>
2024-05-03 12:45:59 +0530

    notifications page

--- frontend/src/components/Navigations.jsx
+++ frontend/src/components/Navigations.jsx
@@ -1,82 +1,97 @@
-import React from "react";
-import Logo from "../assets/twitter-x.svg";
-import { GoHomeFill } from "react-icons/go";
-import { FaRegUser } from "react-icons/fa6";
-import { CgSearch } from "react-icons/cg";
-import { FiBell } from "react-icons/fi";
-import { HiOutlineEnvelope } from "react-icons/hi2";
-import { GoBookmark } from "react-icons/go";
 import { Link } from "react-router-dom";
+// import LoadingSpinner from "../../components/common/LoadingSpinner";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaUser } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
 
-const Navigations = () => {
+const Notifications = () => {
+  const isLoading = false;
+  const notifications = [
+    {
+      _id: "1",
+      from: {
+        _id: "1",
+        username: "johndoe",
+        profileImg: "/avatars/boy2.png",
+      },
+      type: "follow",
+    },
+    {
+      _id: "2",
+      from: {
+        _id: "2",
+        username: "janedoe",
+        profileImg: "/avatars/girl1.png",
+      },
+      type: "like",
+    },
+  ];
+
+  const deleteNotifications = () => {
+    alert("All notifications deleted");
+  };
+
   return (
-    <div className=" hidden lg:w-[32%]  bg-white font-sans ">
-      <div className="ml-[30%] border-2 w-[70%] h-full border-black flex flex-col gap-[18px] pt-3">
-        <div>
-          <img className="ml-5" style={{ width: "36px" }} src={Logo} alt="" />
-        </div>
-        <div className="flex items-center gap-[20px] hover:bg-gray-300 cursor py-2.5 rounded-3xl w-min px-5 cursor-pointer">
-          <div>
-            <GoHomeFill style={{ width: "32px", height: "32px" }} />
-          </div>
-          <div className="text-xl font-semibold">Home</div>
-        </div>
-        <div className="flex items-center gap-[20px] hover:bg-gray-300  py-2.5 rounded-3xl  w-min px-5 cursor-pointer">
-          <div>
-            <FaRegUser style={{ width: "30px", height: "30px" }} />
-          </div>
-          <div className="text-xl font-semibold">Profile</div>
-        </div>
-
-        <div className="flex items-center gap-[20px] hover:bg-gray-300  py-2.5 rounded-3xl  w-min px-5 cursor-pointer">
-          <div>
-            <CgSearch style={{ width: "32px", height: "32px" }} />
-          </div>
-          <div className="text-xl font-semibold">Explore</div>
-        </div>
-        <div className="flex items-center gap-[20px] hover:bg-gray-300  py-2.5 rounded-3xl  w-min px-5 cursor-pointer">
-          <div>
-            <FiBell style={{ width: "32px", height: "32px" }} />
-          </div>
-          <div className="text-xl font-semibold">Notifications</div>
-        </div>
-        <div className="flex items-center gap-[20px] hover:bg-gray-300 py-2.5 rounded-3xl  w-min px-5 cursor-pointer">
-          <div>
-            <HiOutlineEnvelope style={{ width: "32px", height: "32px" }} />
-          </div>
-          <div className="text-xl font-semibold">Messages</div>
-        </div>
-        <div className="flex items-center gap-[20px] hover:bg-gray-300  py-2.5 rounded-3xl  w-min px-5 cursor-pointer">
-          <div>
-            <GoBookmark style={{ width: "32px", height: "32px" }} />
-          </div>
-          <div className="text-xl font-semibold">Bookmarks</div>
-        </div>
-
-        <div className="button mt-4 mb-12 ">
-          <button className="bg-violet-600 hover:bg-violet-700 w-[70%] py-3 rounded-3xl text-lg text-white font-semibold">
-            Post
-          </button>
-        </div>
-        <div className="profile flex items-center gap-[20px] cursor-pointer hover:bg-gray-300 py-3 rounded-3xl">
-          <div>
-            <Link to={"/"}>
-              <img
-                style={{ width: "50px" }}
-                className="rounded-full ml-3"
-                src="https://avatars.githubusercontent.com/u/97403179?s=400&u=ef6d2576225d289c623027bb4f8246251d424cba&v=4"
-                alt=""
-              />
-            </Link>
-          </div>
-          <div>
-            <p className="font-semibold">Rupesh Piwal</p>
-            <p className="text-gray-500">@rupeshstwt</p>
+    <>
+      <div className="flex-[4_4_0] border-l border-r border-gray-700 min-h-screen">
+        <div className="flex justify-between items-center p-4 border-b border-gray-700">
+          <p className="font-bold">Notifications</p>
+          <div className="dropdown ">
+            <div tabIndex={0} role="button" className="m-1">
+              <IoSettingsOutline className="w-4" />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a onClick={deleteNotifications}>Delete all notifications</a>
+              </li>
+            </ul>
           </div>
         </div>
+        {isLoading && (
+          <div className="flex justify-center h-full items-center">
+            <LoadingSpinner size="lg" />
+          </div>
+        )}
+        {notifications?.length === 0 && (
+          <div className="text-center p-4 font-bold">No notifications 🤔</div>
+        )}
+        {notifications?.map((notification) => (
+          <div className="border-b border-gray-700" key={notification._id}>
+            <div className="flex gap-2 p-4">
+              {notification.type === "follow" && (
+                <FaUser className="w-7 h-7 text-primary" />
+              )}
+              {notification.type === "like" && (
+                <FaHeart className="w-7 h-7 text-red-500" />
+              )}
+              <Link to={`/profile/${notification.from.username}`}>
+                <div className="avatar">
+                  <div className="w-8 rounded-full">
+                    <img
+                      src={
+                        notification.from.profileImg ||
+                        "/avatar-placeholder.png"
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="flex gap-1">
+                  <span className="font-bold">
+                    @{notification.from.username}
+                  </span>{" "}
+                  {notification.type === "follow"
+                    ? "followed you"
+                    : "liked your post"}
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 };
-
-export default Navigations;
+export default Notifications;
