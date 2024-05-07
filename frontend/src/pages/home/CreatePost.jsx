8889a8bfc15398de873c8efc6607ec23cb2b25c8
rupesh <rpiwal02@gmail.com>
2024-05-07 10:57:34 +0530

    imported axios

--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
+import axios from 'axios';
 
 const CreatePost = () => {
   const [text, setText] = useState("");
